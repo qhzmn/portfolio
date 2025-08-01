915c506ea9d1008bebe7c53e1a1851c3107f5107
Quentin10 <quentin.hazemann@viacesi.fr>
2025-08-01 17:03:29 +0200

    Version avec json

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,41 +1,43 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("assets/json/projects.json")
+    .then(res => res.json())
+    .then(data => {
+        const projets = data.projects;
+        const container = document.getElementById("projects-all");
+        if (!container) {
+        console.error("Le conteneur projects-all est introuvable dans le HTML.");
+        return;
+        }
+        projets.forEach(projet => {
+            const div = document.createElement("div");
+            div.classList.add("repo");
+            
+            div.innerHTML = `
+            <h2><a href="${projet.url}" target="_blank">${projet.name}</a></h2>
+            <hr>
+            <p>${projet.description || "Aucune description"}</p>
+            <p><strong>Langages :</strong> ${projet.languages.join(", ") || "Inconnu"}</p>
+            <a href="${projet.url}" target="_blank" rel="noopener" class="button-project">Voir sur GitHub</a>
+            <button class="button-project-readme" data-repo="${projet.readme}">Voir README</button>
+            `;
+            container.appendChild(div);
+        });
 
-const container = document.getElementById("repos");
+        document.querySelectorAll(".button-project-readme").forEach(btn => {
+            btn.addEventListener("click", () => {
+            const repo = btn.getAttribute("data-repo");
+            afficherReadme(repo);
+            });
+        });
+    })
+    .catch(err => console.error("Erreur chargement projets.json :", err));
 
 
-fetch(`https://api.github.com/users/${username}/repos?sort=updated`, {
-  headers: {
-      Authorization: `token ${token}`
-    }
-})
-  .then(response => response.json())
-  .then(repos => {
-    repos.forEach(repo => {
-      const div = document.createElement("div");
-      div.className = "repo";
-      div.innerHTML = `
-        
-        <h2><a href="${repo.html_url}" target="_blank">${repo.name}</a></h2>
-        <hr>
-        <p>${repo.description || "Aucune description"}</p>
-        <p>Langage : ${repo.language || "Inconnu"}</p>
-        <p>⭐ ${repo.stargazers_count} | 🍴 ${repo.forks_count}</p>
-        <a href="${repo.html_url}" target="_blank" rel="noopener" class="button-style">Voir le projet sur GitHub</a>
 
-      `;
 
-      const button = document.createElement("button");
-      button.textContent = "Voir le README";
-      button.classList.add("button-style");  // ajoute cette classe
 
-      button.addEventListener("click", () => afficherReadme(repo.name));
-      div.appendChild(button);
+});
 
-      container.appendChild(div);
 
-      
-    });
-  })
-  .catch(error => {
-    container.innerHTML = "<p>Erreur lors du chargement des projets.</p>";
-    console.error(error);
-  });
+
+
--- assets/js/skills.js
+++ assets/js/skills.js
@@ -1,111 +1,25 @@
-
-document.addEventListener("DOMContentLoaded", () => {
-  const username = "qhzmn";
-
-  const groupWeb = ["JavaScript", "HTML", "CSS", "PHP"];
-  const webProjects = [];
-
-  const groupReseaux = [];
-  const reseauxProjects = [];
-
-  const groupDev = ["Python", "C++", "C"];
-  const devProjects = [];
-
-  const groupMethodes = [];
-  const methodesProjects = [];
-
-  const webList = document.getElementById("projects-web");
-  const devList = document.getElementById("projects-dev");
-  const reseauxList = document.getElementById("projects-reseaux");
-  const methodesList = document.getElementById("projects-methodes");
-
-  fetch(`https://api.github.com/users/${username}/repos`, {
-    
-  })
+fetch("assets/json/projects.json")
   .then(res => res.json())
-  .then(async repos => {
-    for (const repo of repos) {
-      const langRes = await fetch(repo.languages_url, {
-        headers: {
-          Authorization: `token ${token}`
-        }
-      });
-      const langs = await langRes.json();
-      const repoLanguages = Object.keys(langs);
+  .then(data => { 
 
-      // Vérifie si au moins un langage est dans groupWeb
-      const hasWebLang = repoLanguages.some(lang => groupWeb.includes(lang));
-      const hasDevLang = repoLanguages.some(lang => groupDev.includes(lang));
-      const hasReseauxLang = repoLanguages.some(lang => groupReseaux.includes(lang));
-      const hasDMethodesLang = repoLanguages.some(lang => groupMethodes.includes(lang));
+    afficherProjetsParDomaine("projects-web", data.projects, data.categories.web);
+    afficherProjetsParDomaine("projects-reseaux", data.projects, data.categories.reseaux);
+    afficherProjetsParDomaine("projects-methodes", data.projects, data.categories.methodes);
+    afficherProjetsParDomaine("projects-dev", data.projects, data.categories.dev);
+  });
 
+function afficherProjetsParDomaine(idContainer, allProjects, projectIds) {
+  const container = document.getElementById(idContainer);
+  if (!container || !allProjects || !projectIds) return;
 
-      if (hasWebLang) {
-        webProjects.push({
-          name: repo.name,
-          url: repo.html_url,
-          languages: repoLanguages
-        });
-      }
-      if (hasDevLang) {
-        devProjects.push({
-          name: repo.name,
-          url: repo.html_url,
-          languages: repoLanguages
-        });
-      }
-      if (hasReseauxLang) {
-        reseauxProjects.push({
-          name: repo.name,
-          url: repo.html_url,
-          languages: repoLanguages
-        });
-      }
-      if (hasDMethodesLang) {
-        methodesProjects.push({
-          name: repo.name,
-          url: repo.html_url,
-          languages: repoLanguages
-        });
-      }
+  container.innerHTML = ""; 
 
+  projectIds.forEach(id => {
+    const projet = allProjects.find(p => p.id === id);
+    if (!projet) return;
+
+    const li = document.createElement("li");
+    li.innerHTML = `<a href="${projet.url}" target="_blank">${projet.name}</a> (${projet.languages.join(", ")})`;
+    container.appendChild(li);
+  });
 }
-
-      reseauxProjects.push(
-      {
-        name: "calendrier-coiffeuse",
-        url: "https://github.com/qhzmn/calendrier-coiffeuse",
-        languages: ["PHP", "CSS", "Twig", "JavaScript", "HTML"]
-      }
-    );
-
-    methodesProjects.push(
-      {
-        name: "calendrier-coiffeuse",
-        url: "https://github.com/qhzmn/calendrier-coiffeuse",
-        languages: ["PHP", "CSS", "Twig", "JavaScript", "HTML"]
-      }
-    );
-
-
-    
-
-    function displayProjects(listContainer, projects) {
-    if (!listContainer) return;
-    listContainer.innerHTML = "";
-    projects.forEach(proj => {
-      const li = document.createElement("li");
-      li.innerHTML = `<a href="${proj.url}" target="_blank">${proj.name}</a> (${proj.languages.join(", ")})`;
-      listContainer.appendChild(li);
-    });
-  }
-
-  // Affichage des listes
-  displayProjects(webList, webProjects);
-  displayProjects(devList, devProjects);
-  displayProjects(methodesList, methodesProjects);
-  //displayProjects(reseauxList, reseauxProjects);
-
-})
-.catch(err => console.error("Erreur GitHub API :", err));
-});
